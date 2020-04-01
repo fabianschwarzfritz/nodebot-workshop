c341446536646092b7d59f4246975a9fc8d39237
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2020-04-01 20:15:49 +0200

    Remove commented instructions

--- 2-servo-wave.js
+++ 2-servo-wave.js
@@ -14,9 +14,3 @@ board.on('ready', function () {
   });
 });
 
-
-// Create a new Servo instance attached to pin 9
-//   » Use servo.sweep to rotate between 0˚ and 180˚
-//   » Use board.wait to schedule a 'reset' callback after 3 seconds
-//   » The 'reset' callback should stop and center the servo
-//   » Check the docs to see how to bring it back into line
